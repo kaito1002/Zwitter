61a46aadd5c5d7b7629692be726d4cd895d601d3
Kaito <mail.kaito03@gmail.com>
2019-09-09 07:50:29 +0900

    implement filter function for rest api

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -12,18 +12,22 @@ class UserViewSet(viewsets.ModelViewSet):
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+    filter_fields = ('grade', 'quarter', )
 
 
 class ExamViewSet(viewsets.ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
+    filter_fields = ('subject', )
 
 
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
+    filter_fields = ('exam', 'poster', )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    filter_fields = ('exam', 'sender', 'bef_comment')
--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'spa',
     'api'
 ]
@@ -132,6 +133,9 @@ STATICFILES_DIRS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend'
+        ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 2
 }
