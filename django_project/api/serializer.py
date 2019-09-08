598fcc3bd1dcc03d36976057b4642f875054a820
Kaito <mail.kaito03@gmail.com>
2019-09-08 23:57:22 +0900

    add related models serializer

--- django_project/api/serializer.py
+++ django_project/api/serializer.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Subject, Exam, Content, Comment
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('name', 'mail', 'number', 'passwd')
+        fields = ('name', 'number', 'coin')
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ('name', 'grade', 'quater')
+
+
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = ('subject', 'year')
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Content
+        fields = ('type', 'data', 'posted_at')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('content', 'posted_at', 'sender', 'receiver')
