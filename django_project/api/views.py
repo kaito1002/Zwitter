e1eb00e6806a183c829ce5a1e0bd88f93f3ffed0
Kaito <mail.kaito03@gmail.com>
2019-09-09 08:04:28 +0900

    fix filter function

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import User, Subject, Exam, Content, Comment
 from .serializer import UserSerializer, SubjectSerializer, ExamSerializer
 from .serializer import ContentSerializer, CommentSerializer
@@ -12,22 +13,26 @@ class UserViewSet(viewsets.ModelViewSet):
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
-    filter_fields = ('grade', 'quarter', )
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('grade', 'quarter', )
 
 
 class ExamViewSet(viewsets.ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
-    filter_fields = ('subject', )
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('subject', )
 
 
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
-    filter_fields = ('exam', 'poster', )
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('exam', 'poster', )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    filter_fields = ('exam', 'sender', 'bef_comment')
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('exam', 'sender', 'bef_comment')
--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -133,9 +133,9 @@ STATICFILES_DIRS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend'
-        ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 2,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+        ],
 }
