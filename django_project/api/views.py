8eefd39adc679fd8fffeb1000dc7729f6b69103c
Kaito <mail.kaito03@gmail.com>
2019-09-09 05:39:21 +0900

    defime views for each model

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -1,9 +1,29 @@
 from rest_framework import viewsets
-
-from .models import User
-from .serializer import UserSerializer
+from .models import User, Subject, Exam, Content, Comment
+from .serializer import UserSerializer, SubjectSerializer, ExamSerializer
+from .serializer import ContentSerializer, CommentSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class ExamViewSet(viewsets.ModelViewSet):
+    queryset = Exam.objects.all()
+    serializer_class = ExamSerializer
+
+
+class ContentViewSet(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
