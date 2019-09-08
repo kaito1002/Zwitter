b61e1720a29f6989463976c8737f21fd17d01cec
Kaito <mail.kaito03@gmail.com>
2019-09-08 17:01:10 +0900

    add user model

--- django_project/api/models.py
+++ django_project/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=32)
+    mail = models.EmailField()
+    number = models.CharField(
+        max_length=32,
+        validators=[MinLengthValidator(6)]
+    )
+    passwd = models.CharField(max_length=20)
