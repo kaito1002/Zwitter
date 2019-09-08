f0241053fb46d70c715408fa1c21850668995f8b
Kaito <mail.kaito03@gmail.com>
2019-09-09 05:44:43 +0900

    change to deal with models for admin

--- django_project/api/admin.py
+++ django_project/api/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Subject, Exam, Content, Comment
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
