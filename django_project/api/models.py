7995cbfa51a7e831364d07df68f77e82ce2c5e15
Kaito <mail.kaito03@gmail.com>
2019-09-09 10:14:49 +0900

    little fix for model

--- django_project/api/models.py
+++ django_project/api/models.py
@@ -6,12 +6,18 @@ from django.contrib.auth.hashers import check_password
 
 
 class User(models.Model):
-    name = models.CharField(max_length=32)
-    number = models.CharField(
-        max_length=32,
-        validators=[MinLengthValidator(6)]
+    name = models.CharField(
+        max_length=15,
+        validators=[MinLengthValidator(1)]
     )
-    passwd = models.CharField(max_length=20)
+    number = models.CharField(
+        max_length=8,
+        validators=[MinLengthValidator(8)]
+    )
+    passwd = models.CharField(
+        max_length=30,
+        validators=[MinLengthValidator(5)]
+    )
     coin = models.IntegerField(default=0)
 
     def __repr__(self):
