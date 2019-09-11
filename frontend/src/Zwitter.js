4b2c14537cc08d896e95d011bb1b9431033adccd
Shigi-p <shigi.522@gmail.com>
2019-09-11 20:59:06 +0900

    add react code

---
+++ frontend/src/Zwitter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+class Zwitter extends React.Component {
+  render() {
+    return (
+      <div className="Zwitter">
+        <h1>This is Zwitter.</h1>
+      </div>
+    )
+  }
+}
+
+export default Zwitter;
