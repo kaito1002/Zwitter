067cd8a16fbb06a19d0cd9ebfcf82f1d30326306
Kaito <mail.kaito03@gmail.com>
2019-10-01 07:27:00 +0900

    implement user related subjects list endpoint

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -10,6 +10,9 @@ from .serializer import PostSerializer, LikeSerializer, ShareSerializer
 from .serializer import GradeSerializer, QuarterSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
+from rest_framework.authtoken.models import Token
+from datetime import datetime
+from django.db.models import Q
 # from rest_framework.authentication import TokenAuthentication
 # from rest_framework.permissions import IsAuthenticated
 
@@ -35,6 +38,48 @@ class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
     filter_backends = [DjangoFilterBackend]
+
+    @action(methods=['GET'], detail=False, url_path='user_related')
+    def subject_list_user_related(self, request):
+        TOKEN = "17a9fe94ee4e1be64f3d4bb89bc90d4c12da3966"
+        user = Token.objects.select_related('user').get(key=TOKEN)
+        period = self.get_period(user=user)
+        quarter = Q(quarter=period["quarter"][0])
+        for i in range(1, len(period["quarter"])):
+            quarter = quarter | Q(quarter=period["quarter"][i])
+
+        proper_quarter = set([_["subject_id"] for _ in Quarter.objects.filter(
+            quarter
+        ).values()])
+
+        proper_grade = set([_["subject_id"] for _ in Grade.objects.filter(
+            grade=period["grade"]
+        ).values()])
+
+        quarter = Q(quarter=period["quarter"][0])
+        for i in range(1, len(period["quarter"])):
+            quarter = quarter | Q(quarter=period["quarter"][i])
+
+        subjects = list(proper_grade & proper_quarter)
+
+        subject = Q(id=subjects[0])
+        for i in range(1, len(subjects)):
+            subject = subject | Q(id=subjects[i])
+
+        return Response(Subject.objects.filter(
+            subject
+        ).values())
+
+    def get_period(self, user: User, time=datetime.now()):
+        """
+        user => grade
+        time => quarter
+        """
+        # とりあえずサンプル値を返す
+        return {
+            'grade': 1,
+            'quarter': ["前期", "1学期"]
+        }
 
 
 class GradeViewSet(viewsets.ModelViewSet):
@@ -91,7 +136,3 @@ class ShareViewSet(viewsets.ModelViewSet):
     serializer_class = ShareSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ('post')
-
-
-# from rest_framework.authtoken.models import Token
-# Token.objects.select_related('user').get(key=TOKEN).user_id
