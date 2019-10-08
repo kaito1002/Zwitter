df753370edc97cb07c2634514f9c68f279195be7
Kaito <mail.kaito03@gmail.com>
2019-10-08 23:33:25 +0900

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ sudo docker-compose down
 
 if [ ${OS} = "Linux" ]; then
   cd ${PROJECT}
-  git fetch && git reset --hard origin/master
+  git fetch && git reset --hard ogrigin/master
+  sed -i -e "s/DEBUG = True/DEBUG = False/g" django_project/config/settings.py
   cat docker_external.txt >> docker-compose.yml
   cd frontend
   npm install
